c89d2459afa9867f42c3bca68b441cd92d445e25
leroi <leroidejesa@gmail.com>
2015-06-02 14:35:35 -0700

    find and fix a few minor bugs/typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-function Address(street, city, state) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
-}
-
-Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
-}
-
 function Contact(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
@@ -19,6 +9,24 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+};
+
+function resetFields(){
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+  $("div.new-address").not(':first').remove();
+}
 
 $(document).ready(function() {
   // in order to have multiple addresses per contact, adds more fields
@@ -52,7 +60,7 @@ $(document).ready(function() {
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
 
-      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
       newContact.addresses.push(newAddress);
     });
 
@@ -66,16 +74,12 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
 
       $("ul#addresses").text("");
+
       newContact.addresses.forEach(function(address) {
         $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
       });
-
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
   });
 });
